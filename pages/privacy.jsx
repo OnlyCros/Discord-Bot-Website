32e06fca05292fb87f1f33408fa31ca51300ac7b
OnlyCros <138124191+OnlyCros@users.noreply.github.com>
2023-06-29 15:57:54 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -13,7 +13,7 @@ export default function Privacy() {
         <div className="py-3"></div>
         <p>⊳ You cannot use certain commands at the same time while using Astra.</p>   
         <div className="py-3"></div>
-        <p>⊳ Astra bot does not save any data based on users.</p>
+        <p>⊳ Reis Admin bot does not save any data based on users.</p>
         <div className="py-3"></div>
         <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
     
